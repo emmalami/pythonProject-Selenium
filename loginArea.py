e48c71c17a0a6ebd83481d0b736f99e20642d056
Olamide John <olamide.john@outlook.com>
2022-11-01 18:06:58 +0100

    test

--- loginArea.py
+++ loginArea.py
@@ -1,18 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.edge.service import Service
-from webdriver_manager.microsoft import EdgeChromiumDriverManager
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def main():
-    driver = webdriver.Edge(service=Service(EdgeChromiumDriverManager().install()))
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://admin-staging.payfi.ng/")
     login = driver.find_element(By.NAME, "email").send_keys("olamide.john@outlook.com")
     password = driver.find_element(By.NAME, "password").send_keys("Testqa1101$")
-    button = driver.find_element(By.CSS_SELECTOR, "#root > div > div:nth-child(2) > div > div > form > div > div.col-md-6.col-12.register-form-view-cover-row-form > div > button")
-    links = driver.find_elements(By.TAG_NAME, "a")
-    for link in links:
-        print("link", link.text)
+    button = driver.find_element(By.CSS_SELECTOR, "#root > div > div:nth-child(2) > div > div > form > div > "
+                                                  "div.col-md-6.col-12.register-form-view-cover-row-form > div > "
+                                                  "button").click()
+    findXpath = driver.find_element(By.XPATH, "//form[1]")
+    print(findXpath)
+    sleep(30)
+
+    # links = driver.find_elements(By.TAG_NAME, "a")
+    # print("link", links)
     driver.close()
 
 
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
     driver.get("https://admin-staging.payfi.ng/")
-    sleep(30)
+    login = driver.find_element(By.NAME, "email").send_keys("olamide.john@outlook.com")
+    password = driver.find_element(By.NAME, "password").send_keys("Testqa1101$")
+    button = driver.find_element(By.CSS_SELECTOR, "#root > div > div:nth-child(2) > div > div > form > div > div.col-md-6.col-12.register-form-view-cover-row-form > div > button")
+    links = driver.find_elements(By.TAG_NAME, "a")
+    for link in links:
+        print("link", link.text)
     driver.close()
 
-
-main()
