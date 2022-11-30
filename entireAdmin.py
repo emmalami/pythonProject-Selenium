0cd2fc2be1d7d97b6c7c9273a2705d9c327ab117
Olamide John <olamide.john@outlook.com>
2022-11-30 16:02:31 +0100

    PYTHON

---
+++ entireAdmin.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+
+class AppDynamicsJob(unittest.TestCase):
+    def setUp(self):
+        # AppDynamics will automatically override this web driver
+        # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+
+    def test_app_dynamics_job(self):
+        driver = self.driver
+        name("passworddriver.get("https://admin.payfi.ng/")
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("Olamide.john@payfi.ng")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("Testqa1101$")
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/form/div/div[2]/div").click()
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").click()
+        # ERROR: Caught exception [ERROR: Unsupported command [doubleClick | name=email | ]]
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("olamide.john@payfi.ng")
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/form/div/div[2]/div/button").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div/div[2]/ul/li[2]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[2]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[3]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[4]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[5]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[6]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/button/span").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div/span[2]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/button/span").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/button/span").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/ul/li").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/ul/li[2]").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[3]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[2]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[3]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[4]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[5]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[6]").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[2]/a/p").click()
+        driver.find_element_by_name("search").click()
+        driver.find_element_by_name("search").clear()
+        driver.find_element_by_name("search").send_keys("olamide.john@payfi.ng")
+        driver.find_element_by_name("search").send_keys(Keys.ENTER)
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[3]/div/div/div/div/table/tbody/tr/td[3]/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[2]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[5]/div/div/div/div/div/p[2]").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[3]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[4]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[5]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[6]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[7]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[8]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[9]/a/p").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[3]/a/p").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[4]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div/div/p[2]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div/div/p[3]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div/div/p[4]").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[5]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[3]/div/div/div/div/table/tbody/tr/td/p").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[6]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[3]/div/div/div/div/table/tbody/tr/td/p").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[7]/a/p").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[8]/a/p").click()
+        driver.find_element_by_xpath("//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[7]/a/p").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[2]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[3]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[4]").click()
+        driver.find_element_by_xpath(
+            "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[3]/div[2]/ul/li/a/p").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("Olamide.john@payfi.ng")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_").send_keys("Testqa1101$")
+
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
+
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
+
+    def tearDown(self):
+        # To know more about the difference between verify and assert,
+        # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
+        self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- loginArea.py
+++ loginArea.py
@@ -8,18 +8,102 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def main():
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.get("https://admin-staging.payfi.ng/")
+    driver.get("https://admin.payfi.ng/")
     driver.maximize_window()
-    driver.implicitly_wait(20)
+    driver.implicitly_wait(50)
     action = ActionChains(driver)
-    email = driver.find_element(By.NAME, "email").send_keys("olamide.john@payfi.ng")
-    password = driver.find_element(By.NAME, "password").send_keys("Testqa1101$")
-    loginbutton = driver.find_element(By.CSS_SELECTOR, "#root > div > div:nth-child(2) > div > div > form > div > "
-                                                  "div.col-md-6.col-12.register-form-view-cover-row-form > div > "
-                                                  "button").click()
-    customermenu = driver.find_element(By.XPATH,"(//p[contains(@class,'nav-p')][normalize-space()='Customers'])[1]").click()
-    sleep(30)
-    driver.close()
+    driver.find_element(By.NAME, "email").send_keys("Olamide.john@payfi.ng")
+    driver.find_element(By.NAME, "password").send_keys("Testqa1101$")
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/form/div/div[2]/div").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/form/div/div[2]/div/button").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div/div[2]/ul/li[2]/a/p").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div["
+                                  "2]/div/div/p[2]").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div["
+                                  "2]/div/div/p[3]").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div["
+                                  "2]/div/div/p[4]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[5]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[6]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/button/span").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div/div/span[2]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/button/span").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/button/span").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/ul/li").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[2]/div/ul/li[2]").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[3]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[2]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[3]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[4]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[5]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[6]").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[2]/a/p").click()
+    driver.find_element(By.NAME, "search").click()
+    driver.find_element(By.NAME, "search").send_keys("olamide.john@payfi.ng")
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div["
+                                  "3]/div/div/div/div/table/tbody/tr/td[3]/p").click()
+    driver.find_element(By.XPATH , "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div["
+                                  "2]/a/p").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div["
+                                  "5]/div/div/div/div/div/p[2]").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[3]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[4]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[5]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[6]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[7]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[8]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div[4]/div/div[9]/a/p").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[3]/a/p").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[4]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div/div/p[2]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div/div/p[3]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div/div[2]/div/div/p[4]").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[5]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[3]/div/div/div/div/table/tbody/tr/td/p").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[6]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[3]/div/div/div/div/table/tbody/tr/td/p").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[7]/a/p").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[8]/a/p").click()
+    driver.find_element(By.XPATH, "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[2]/ul/li[7]/a/p").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[2]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[3]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div[2]/div[2]/div/div/div/div/div[2]/div/div/p[4]").click()
+    driver.find_element(By.XPATH,
+                        "//div[@id='root']/div/div[2]/div/div/div/div/div/div/div[3]/div[2]/ul/li/a/p").click()
+    driver.find_element(By.NAME, "email").clear()
+    driver.find_element(By.NAME, "email").send_keys("Olamide.john@payfi.ng")
+    driver.find_element(By.NAME, "password").clear()
 
 
-main()
+if __name__ == "__main__":
+    main()
